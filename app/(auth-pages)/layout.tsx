06076bec1628cf6a5c1462a888be2797e3a42bc4
LeonKant <lkantikov20@gmail.com>
2024-11-13 16:59:25 -0800

    init setup

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,9 +1,15 @@
+import DefaultWrapper from "@/components/default-wrapper";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
+    <DefaultWrapper>
+      <div className="max-w-7xl flex flex-col gap-12 items-start">
+        {children}
+      </div>
+    </DefaultWrapper>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import DefaultWrapper from "@/components/default-wrapper";
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
@@ -5,12 +6,12 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
-    <>
+    <DefaultWrapper>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
-    </>
+    </DefaultWrapper>
   );
 }
