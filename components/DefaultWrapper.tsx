41929f36016e70f65475707cab7cf2e7e580ab4c
LeonKant <lkantikov20@gmail.com>
2024-11-25 16:56:07 -0800

    chore: rename component file

--- components/DefaultWrapper.tsx
+++ components/DefaultWrapper.tsx
@@ -5,10 +5,15 @@ import { EnvVarWarning } from "./env-var-warning";
 import { ModeToggle } from "./theme-toggle";
 import HeaderAuth from "@/components/header-auth";
 
-export default async function DefaultWrapper({ children }: PropsWithChildren) {
+export default function DefaultWrapper({
+  centerChildren = false,
+  children,
+}: { centerChildren?: boolean } & PropsWithChildren) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col justify-between items-center">
+      <div
+        className={`flex-1 w-full flex flex-col justify-between ${centerChildren && "items-center"}`}
+      >
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-fit">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
@@ -22,7 +27,12 @@ export default async function DefaultWrapper({ children }: PropsWithChildren) {
             </div>
           </div>
         </nav>
-        <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+        <div
+          className={`flex ${centerChildren ? "flex-col gap-20 max-w-5xl p-5" : "flex-1"}`}
+        >
+          {children}
+        </div>
+        {/* <div className="flex-1">{children}</div> */}
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16"></footer>
       </div>
     </main>
