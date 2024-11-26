b5e13b31ee7b9edfb154e102f19fb442d0fac0f6
LeonKant <lkantikov20@gmail.com>
2024-11-25 16:59:43 -0800

    fix: prevent reconnnect to database for hot reload

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,33 @@
+// import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+
+// const connectionString = process.env.DATABASE_URL ?? "";
+
+// console.log("new postgres connection:", connectionString)
+
+// // Disable prefetch as it is not supported for "Transaction" pool mode
+// export const client = postgres(connectionString, { prepare: false });
+// export const db = drizzle(client);
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const connectionString = process.env.DATABASE_URL ?? "";
+declare namespace global {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+let postgresSqlClient;
+
+const databaseUrl = process.env.DATABASE_URL ?? "";
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.postgresSqlClient) {
+    console.log("new postgres connection:", databaseUrl)
+    global.postgresSqlClient = postgres(databaseUrl);
+  }
+  postgresSqlClient = global.postgresSqlClient;
+} else {
+  postgresSqlClient = postgres(databaseUrl);
+}
+
+export const db = drizzle(postgresSqlClient);
