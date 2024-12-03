f6d0f1554c41cdddbfe4afac32ae38bfb39df720
LeonKant <lkantikov20@gmail.com>
2024-12-03 15:57:01 -0800

    feat(form): gameLobbySchema updates

--- lib/form-schemas/GameLobbyScema.ts
+++ lib/form-schemas/GameLobbyScema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const gameLobbySchema = z.object({
   gameInput: z
     .string()
-    .regex(/[A-Za-z]/, "Input must be a letter")
-    .transform((val) => val.toLowerCase()),
+    .min(1,"")
+    .regex(/[A-Za-z]/, "Input must be a letter"),
 });
