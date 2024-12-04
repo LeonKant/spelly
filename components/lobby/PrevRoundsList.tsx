d284a4b92ce64e9a292ea2507f7b16cfc4e1d166
LeonKant <lkantikov20@gmail.com>
2024-12-04 12:46:06 -0800

    init: PrevRoundsList component

---
+++ components/lobby/PrevRoundsList.tsx
@@ -0,0 +1,15 @@
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
+
+export function PrevRoundsList() {
+
+  
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton className="cursor-default">
+          <span className="text-base">Test</span>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
