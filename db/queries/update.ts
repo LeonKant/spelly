d06eaf4429b2fbdf375bc8db0af5805e5ed39804
LeonKant <lkantikov20@gmail.com>
2024-12-06 16:23:26 -0800

    feat(db): generic updateLobbyState function; increment player points function

---
+++ db/queries/update.ts
@@ -0,0 +1,30 @@
+import { and, eq, sql } from "drizzle-orm";
+import { db } from "..";
+import {
+  lobbiesInSpelly,
+  lobbyPlayersInSpelly,
+  SpellyLobbyT,
+} from "../schema/spelly";
+
+export async function updateLobbyState(
+  lobbyId: string,
+  lobbyInfo: Omit<Partial<SpellyLobbyT>, "hostId" | "id" | "lobbyPlayerIds">,
+) {
+  await db
+    .update(lobbiesInSpelly)
+    .set({ ...lobbyInfo })
+    .where(eq(lobbiesInSpelly.id, lobbyId));
+}
+
+export async function incrementPlayerPoints(userId: string, lobbyId: string) {
+  await db.update(lobbyPlayersInSpelly)
+    .set({
+      points: sql`${lobbyPlayersInSpelly.points} + 1`,
+    })
+    .where(
+      and(
+        eq(lobbyPlayersInSpelly.userId, userId),
+        eq(lobbyPlayersInSpelly.lobbyId, lobbyId),
+      ),
+    );
+}
