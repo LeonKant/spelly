d74e6188ad634246b338ad772548580b866f1414
LeonKant <lkantikov20@gmail.com>
2024-12-06 16:05:59 -0800

    refactor: GameLobbyForm to use LobbyContext
    feat: focus if user's turn; reset form if game state changes

--- components/forms/GameLobbyForm.tsx
+++ components/forms/GameLobbyForm.tsx
@@ -1,27 +1,38 @@
 "use client";
 
+import { playerTurnSubmitAction, startGameAction } from "@/actions/lobby";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { SpellyLobbyT } from "@/db/schema/spelly";
+import { useSpellyLobby } from "@/context/LobbyContext";
 import { gameLobbySchema } from "@/lib/form-schemas/GameLobbyScema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormEvent } from "react";
+import { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-interface Props {
-  lobbyState: SpellyLobbyT;
-  userID: string;
-}
-export default function GameLobbyForm({ lobbyState, userID }: Props) {
-  const { gameState, lobbyPlayerIds, currentPlayer } = lobbyState;
+export default function GameLobbyForm() {
+  const {
+    userID,
+    lobbyState: {
+      gameState,
+      lobbyPlayerIds,
+      currentPlayer,
+      gameStarted,
+      hostId,
+    },
+  } = useSpellyLobby();
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  if (lobbyPlayerIds[currentPlayer] === userID) {
+    inputRef.current?.focus();
+  }
 
   const gameLobbyFormReturn = useForm<z.infer<typeof gameLobbySchema>>({
     resolver: zodResolver(gameLobbySchema),
@@ -29,25 +40,35 @@ export default function GameLobbyForm({ lobbyState, userID }: Props) {
     defaultValues: { gameInput: "" },
   });
 
+  const currentUserTurn = userID === lobbyPlayerIds[currentPlayer];
+
+  useEffect(() => {
+    gameLobbyFormReturn.reset();
+  }, [gameState]);
+
   return (
     <Form {...gameLobbyFormReturn}>
       <form
         className="mx-1 flex flex-col items-center gap-4"
-        onSubmit={(e: FormEvent<HTMLFormElement>) => {
-          e.preventDefault();
-        }}
+        autoComplete="off"
+        onSubmit={gameLobbyFormReturn.handleSubmit(playerTurnSubmitAction)}
       >
         <FormField
           name="gameInput"
           control={gameLobbyFormReturn.control}
-          render={({ field }) => (
+          render={({ field: { ref, ...rest } }) => (
             <FormItem className="w-full">
               <FormControl>
                 <div className="flex w-full items-center rounded-md border-2 border-input bg-background px-3 py-2 text-2xl ring-offset-background has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50 has-[:focus]:outline-none has-[:focus]:ring-2 has-[:focus]:ring-ring has-[:focus]:ring-offset-2">
                   {gameState}
                   <input
-                    disabled={userID !== lobbyPlayerIds[currentPlayer]}
-                    {...field}
+                    inputMode="text"
+                    disabled={!currentUserTurn || !gameStarted}
+                    ref={(e) => {
+                      ref(e);
+                      inputRef.current = e;
+                    }}
+                    {...rest}
                     maxLength={1}
                     className={`border-none bg-inherit outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-60`}
                   />
@@ -57,13 +78,23 @@ export default function GameLobbyForm({ lobbyState, userID }: Props) {
             </FormItem>
           )}
         />
-        <Button
-          type="submit"
-          className="w-fit text-lg"
-          disabled={!gameLobbyFormReturn.formState.isValid}
-        >
-          Submit
-        </Button>
+        {!gameStarted && userID === hostId ? (
+          <Button className="w-fit text-lg" onClick={() => startGameAction()}>
+            Start Game
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            className="w-fit text-lg"
+            disabled={
+              !gameStarted ||
+              !currentUserTurn ||
+              !gameLobbyFormReturn.formState.isValid
+            }
+          >
+            Submit
+          </Button>
+        )}
       </form>
     </Form>
   );
