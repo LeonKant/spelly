4a4b258e85b8eefc1e38d103ad6d21b847c2338d
LeonKant <lkantikov20@gmail.com>
2024-12-10 16:30:35 -0800

    style: reduce size of sidebar trigger button; display spinner if waiting for subscription to lobby

--- components/lobby/Lobby.tsx
+++ components/lobby/Lobby.tsx
@@ -3,14 +3,27 @@ import GameLobbyForm from "../forms/GameLobbyForm";
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import { LobbyPlayerList } from "./LobbyPlayerList";
 import { GameOverDialog } from "./GameOverDialog";
+import { useSpellyLobby } from "@/context/LobbyContext";
+import { LoaderCircle } from "lucide-react";
 
 export default function Lobby() {
+  const { subscriptionWaiting } = useSpellyLobby();
+
+  if (subscriptionWaiting) {
+    return (
+      <div className="flex flex-1 items-center justify-center gap-2">
+        <span>Loading</span>
+        <LoaderCircle className="w-fit animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div
       className={`mx-auto flex max-w-screen-xl flex-1 flex-col justify-between p-8 lg:flex-row`}
     >
       <div className={`min-w-fit basis-1/5`}>
-        <SidebarTrigger className="[&_svg]:shrink-1 min-h-fit min-w-fit p-2 text-lg [&_svg]:size-8" />
+        <SidebarTrigger className="[&_svg]:shrink-1 min-h-fit min-w-fit p-2 text-lg [&_svg]:size-7" />
       </div>
       <div className="my-8 flex w-full basis-3/5 flex-col justify-center items-center">
         <GameLobbyForm />
